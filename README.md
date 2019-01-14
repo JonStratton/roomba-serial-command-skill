99ae0b87f83329cc3d6febe2a175ce23817905d0
jgstratton <jgstratt@sdf.org>
2019-01-13 19:53:35 -0600

    Got skill basically working.

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-## Roomba Serial Command
+# <img src='https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/robot.svg' card_color='#40DBB0' width='50' height='50' style='vertical-align:bottom'/> Roomba Serial Command
 Talks to a roomba that is connected to something running roomba-serial-command-service.
 
-## Description
+## About
 This skill is to be used with a roomba with a serial connection to a small single board computer running the roomba-serial-command-service. this device must also be connected to the same local network that mycroft is running from.
 
 ## Examples
- - "Tell roomba to clean"
- - "Tell roomba to dock"
- - "Tell roomba to turn off"
-
+* "List robots"
+* "Tell roomba to clean"
+* "Tell roomba to dock"
+* "Tell robot off"
 
 ## Credits
 Jon Stratton
 
+## Category
+
+## Tags
 
--- __init__.py
+++ __init__.py
@@ -1,14 +1,37 @@
-from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+from zeroconf import ServiceBrowser, Zeroconf
+import urllib.request
 
+roomba_list = []
+
+class MyListener:
+    def remove_service(self, zeroconf, type, name):
+        roomba_list.remove(name.split('.')[0])
+    def add_service(self, zeroconf, type, name):
+        roomba_list.append(name.split('.')[0])
 
 class RoombaSerialCommand(MycroftSkill):
+
     def __init__(self):
         MycroftSkill.__init__(self)
+        zeroconf = Zeroconf()
+        listener = MyListener()
+        browser = ServiceBrowser(zeroconf, '_roomba._tcp.local.', listener)
 
-    @intent_file_handler('command.serial.roomba.intent')
-    def handle_command_serial_roomba(self, message):
-        self.speak_dialog('command.serial.roomba')
+    @intent_handler(IntentBuilder('ListRoomba').require('List').require('Roomba'))
+    def handle_list_roomba(self, message):
+        self.speak( ', '.join(map(str, roomba_list)) )
 
+    @intent_handler(IntentBuilder('TellRoombaTo').require('Tell').require('Roomba').require('ToCommand'))
+    def handle_tell_roomba_to(self, message):
+        if roomba_list: # TODO, multiple roombas
+            roomba_command_url = 'http://%s/do/%s/' % ( roomba_list[0], message.data.get('ToCommand') )
+            response = urllib.request.urlopen(roomba_command_url)
+            roomba_response = response.read().decode('utf-8')
+            self.speak_dialog('roomba.responds', data={'roomba_response':roomba_response})
+        else:
+            self.speak_dialog('no.roomba')
 
 def create_skill():
     return RoombaSerialCommand()
